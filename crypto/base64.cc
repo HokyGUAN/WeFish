7e46a06a8619a82f9f6b97fbe04ab5adebc1fb52
HokyGUAN <hoky.guan@tymphany.com>
2024-01-30 17:31:28 +0800

    overwrite base64 decode/encode

--- crypto/base64.cc
+++ crypto/base64.cc
@@ -1,127 +1,91 @@
-#include "crypto.hpp"
+#include "base64.hpp"
 
-unsigned char CBASE64::s_encTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-unsigned char CBASE64::s_decTable[] = {
-    0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
-    0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
-    0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x3E,0xFF,0xFF,0xFF,0x3F,
-    0x34,0x35,0x36,0x37,0x38,0x39,0x3A,0x3B,0x3C,0x3D,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
-    0xFF,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,
-    0x0F,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0xFF,0xFF,0xFF,0xFF,0xFF,
-    0xFF,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,
-    0x29,0x2A,0x2B,0x2C,0x2D,0x2E,0x2F,0x30,0x31,0x32,0x33,0xFF,0xFF,0xFF,0xFF,0xFF
-};
 
-std::string CBASE64::encode(const std::string& str)
+static const std::string base64_chars =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789+/";
+
+static inline bool isBase64(unsigned char c) {
+	return (isalnum(c) || (c == '+') || (c == '/'));
+}
+std::string CBASE64::base64Encode(const char* bytes_to_encode, unsigned int in_len)
 {
-    std::string strEncode;
-    strEncode.resize( (str.size() / 3 + 1) * 4 );
-    strEncode.resize( __encode((unsigned char*)strEncode.data(), (const unsigned char*)str.data(), str.size()) );
-    return strEncode;
+   std::string ret;
+	int i = 0;
+	int j = 0;
+	unsigned char char_array_3[3];
+	unsigned char char_array_4[4];
+
+	while (in_len--) {
+		char_array_3[i++] = *(bytes_to_encode++);
+		if (i == 3) {
+			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+			char_array_4[3] = char_array_3[2] & 0x3f;
+
+			for (i = 0; (i < 4); i++)
+				ret += base64_chars[char_array_4[i]];
+			i = 0;
+		}
+	}
+
+	if (i)
+	{
+		for (j = i; j < 3; j++)
+			char_array_3[j] = '\0';
+
+		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+		for (j = 0; (j < i + 1); j++)
+			ret += base64_chars[char_array_4[j]];
+
+		while ((i++ < 3))
+			ret += '=';
+
+	}
+
+	return ret;
 }
 
-std::string CBASE64::decode(const std::string& str)
+std::string CBASE64::base64Decode(const std::string & encoded_string)
 {
-    std::string strDecode;
-    strDecode.resize( str.size() );
-    strDecode.resize( __decode((unsigned char*)strDecode.data(), (const unsigned char*)str.data(), str.size()) );
-    return strDecode;
+    int in_len = encoded_string.size();
+	int i = 0;
+	int j = 0;
+	int in_ = 0;
+	unsigned char char_array_4[4], char_array_3[3];
+	std::string ret;
+
+	while (in_len-- && (encoded_string[in_] != '=') && isBase64(encoded_string[in_])) {
+		char_array_4[i++] = encoded_string[in_]; in_++;
+		if (i == 4) {
+			for (i = 0; i < 4; i++)
+				char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+			for (i = 0; (i < 3); i++)
+				ret += char_array_3[i];
+			i = 0;
+		}
+	}
+
+	if (i) {
+		for (j = 0; j < i; j++)
+			char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+		char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+		char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+
+		for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+	}
+
+	return ret;
 }
 
-int CBASE64::__encode(unsigned char* pDest, const unsigned char* pSrc, size_t nSrcLen)
-{
-    unsigned char * dst = pDest;
-    const unsigned char * src = pSrc;
-    size_t len = nSrcLen;
-
-    unsigned char * odst = dst;
-    unsigned long l = 0, m = 0, n = 0;
-
-    // 循环处理分组
-    size_t off = 0;
-    for ( ; off + 3 <= len; off += 3) {
-        l = *src++;
-        m = *src++;
-        n = *src++;
-
-        *dst++ = s_encTable[ (l >> 2) & 0x3F ];
-        *dst++ = s_encTable[ ((l << 4) & 0x30) | ((m >> 4) & 0x0F) ];
-        *dst++ = s_encTable[ ((m << 2) & 0x3C) | ((n >> 6) & 0x03) ];
-        *dst++ = s_encTable[ n & 0x3F ];
-    }
-
-    // 处理余下的2个字节
-    if (off + 2 <= len) {
-        l = *src++;
-        m = *src++;
-
-        *dst++ = s_encTable[ (l >> 2) & 0x3F ];
-        *dst++ = s_encTable[ ((l << 4) & 0x30) | ((m >> 4) & 0x0F) ];
-        *dst++ = s_encTable[ ((m << 2) & 0x3C) ];
-        *dst++ = '=';
-    } else if (off + 1 <= len) {
-        l = *src++;
-
-        *dst++ = s_encTable[ (l >> 2) & 0x3F ];
-        *dst++ = s_encTable[ ((l << 4) & 0x30) ];
-        *dst++ = '=';
-        *dst++ = '=';
-    }
-
-    return (int)(dst - odst);
-}
-
-// 分组解码
-int CBASE64::__decode(unsigned char* pDest, const unsigned char* pSrc, size_t nSrcLen)
-{
-    unsigned char * dst = pDest;
-    const unsigned char * src = pSrc;
-    size_t len = nSrcLen;
-
-    unsigned char * odst = dst;
-    unsigned long l = 0, m = 0, n = 0, o = 0;
-
-    // 循环处理分组
-    size_t off = 0;
-    for (; off + 4 <= len; off += 4) {
-        if ( (src[0] > 0x7F) || (src[1] > 0x7F) || (src[2] > 0x7F) || (src[3] > 0x7F) ) return (int)(dst - odst);
-        if ( (src[0] == '=') || (src[1] == '=') || (src[2] == '=') || (src[3] == '=') ) break;
-
-        l = s_decTable[*src++];
-        m = s_decTable[*src++];
-        n = s_decTable[*src++];
-        o = s_decTable[*src++];
-
-        *dst++ = (unsigned char)( ((l << 2) & 0xFC) | ((m >> 4) & 0x03) );
-        *dst++ = (unsigned char)( ((m << 4) & 0xF0) | ((n >> 2) & 0x0F) );
-        *dst++ = (unsigned char)( ((n << 6) & 0xC0) | (o & 0x3F));
-    }
-
-    // 处理余下的3个字节
-    if (off + 3 <= len) {
-        if ( (src[0] != '=') && (src[1] != '=') ) {
-            l = s_decTable[*src++];
-            m = s_decTable[*src++];
-
-            *dst++ = (unsigned char)( ((l << 2) & 0xFC) | ((m >> 4) & 0x03) );
-        }
-
-        if ( (src[2] != '=') ){
-            n = s_decTable[*src++];
-
-            *dst++ = (unsigned char)( ((m << 4) & 0xF0) | ((n >> 2) & 0x0F) );
-        }
-    }
-
-    // 处理余下的两个字节
-    else if (off + 2 <= len) {
-        if ( (src[0] != '=') && (src[1] != '=') ) {
-            l = s_decTable[*src++];
-            m = s_decTable[*src++];
-
-            *dst++ = (unsigned char)( ((l << 2) & 0xFC) | ((m >> 4) & 0x03) );
-        }
-    }
-
-    return (int)(dst - odst);
-}
--- crypto/base64.hpp
+++ crypto/base64.hpp
@@ -1,23 +1,15 @@
-#ifndef CRYPTO_H
-#define CRYPTO_H
+#ifndef CBASE64_H
+#define CBASE64_H
 
 #include <string>
 
 class CBASE64
 {
 public:
-    static std::string encode(const std::string& str);
+    static std::string base64Encode(const char* bytes_to_encode, unsigned int in_len);
 
-    static std::string decode(const std::string& str);
+    static std::string base64Decode(const std::string & encoded_string);
 
-private:
-    static int __encode(unsigned char* pDest, const unsigned char* pSrc, size_t nSrcLen);
-
-    static int __decode(unsigned char* pDest, const unsigned char* pSrc, size_t nSrcLen);
-
-private:
-    static unsigned char s_encTable[];
-    static unsigned char s_decTable[];
 };
 
-#endif // CRYPTO_H
+#endif // CBASE64_H
